68aebd1a0ed0f6b2b1dc3f348d6abfe7ce339d0d
khj1 <74335027+khj1@users.noreply.github.com>
2021-10-06 19:37:32 +0900

    feat : 요구사항 2번 구현
    구현 내용
        - 같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 포볼또는 낫싱을 반환한다.
        - 해당 요구사항에 대한 단위 테스트 진행

--- src/main/java/bullsandcows/BullsAndCows.java
+++ src/main/java/bullsandcows/BullsAndCows.java
@@ -1,24 +1,68 @@
 package bullsandcows;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class BullsAndCows {
     private static final Random random = new Random();
 
-    public ArrayList<Integer> getRnd() {
-        ArrayList<Integer> ints = new ArrayList<>();
+    public List<Integer> getRnd() {
+        List<Integer> ints = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             ints.add(deduplication(ints));
         }
         return ints;
     }
 
-    public int deduplication(ArrayList<Integer> ints) {
+    private int deduplication(List<Integer> ints) {
         int n = random.nextInt(8) + 1;
         while (ints.contains(n)) {
             n = random.nextInt(8) + 1;
         }
         return n;
     }
+
+    public List<Integer> getIntegers(String input) {
+        int n = Integer.parseInt(input);
+        List<Integer> ints = new ArrayList<>();
+
+        ints.add(n / 100);
+        ints.add(n % 100 / 10);
+        ints.add(n % 10);
+
+        return ints;
+    }
+
+    public Map<String, Integer> getResult(List<Integer> rnd, String input) {
+        List<Integer> integers = getIntegers(input);
+
+        Map<String, Integer> result = new HashMap<>();
+
+        int ball = 0;
+        int strike = 0;
+
+        for (int i = 0; i < 3; i++) {
+            ball += getBall(rnd, integers, i);
+            strike += getStrike(rnd, integers, i);
+        }
+
+        result.put("strike", strike);
+        result.put("ball", ball - strike);
+
+        return result;
+    }
+
+    private int getBall(List<Integer> rnd, List<Integer> integers, int i) {
+        if (rnd.contains(integers.get(i))) {
+            return 1;
+        }
+        return 0;
+    }
+
+    private int getStrike(List<Integer> rnd, List<Integer> integers, int i) {
+        if (rnd.get(i).equals(integers.get(i))) {
+            return 1;
+        }
+        return 0;
+    }
+
 }
--- src/test/java/bullsandcows/BullsAndCowsTest.java
+++ src/test/java/bullsandcows/BullsAndCowsTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,7 +24,7 @@ class BullsAndCowsTest {
     @Test
     @DisplayName(value = "컴퓨터는_임의의_수_3개를_선택한다")
     void getRndTest() {
-        ArrayList<Integer> rnd = bullsAndCows.getRnd();
+        List<Integer> rnd = bullsAndCows.getRnd();
 
         assertThat(rnd.size()).isEqualTo(3);
 
@@ -35,4 +37,27 @@ class BullsAndCowsTest {
         assertThat(rnd.get(2)).isBetween(1, 9);
     }
 
+    @Test
+    @DisplayName(value = "플레이어가 입력한 문자열을 정수 리스트로 변환")
+    void getIntegers() {
+        String value = "123";
+        List<Integer> integers = bullsAndCows.getIntegers(value);
+
+        assertThat(integers).containsSequence(1, 2, 3);
+    }
+
+    @Test
+    @DisplayName(value = "자릿수가 일치하면 strike, 값을 포함하면 ball 반환")
+    void getResult() {
+        List<Integer> rnd = new ArrayList<>();
+        rnd.add(3);
+        rnd.add(4);
+        rnd.add(2);
+
+        String input = "241";
+        Map<String, Integer> result = bullsAndCows.getResult(rnd, input);
+
+        assertThat(result).extracting("ball").isEqualTo(1);
+        assertThat(result).extracting("strike").isEqualTo(1);
+    }
 }
